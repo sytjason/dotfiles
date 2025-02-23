362da3077ec6c5273327c06f1956dd87645114c3
sytjason <jasont1020508@gmail.com>
2025-02-23 17:36:22 +0800

    [eww] reveal full time output when hover

--- .config/eww/scripts/get-time.sh
+++ .config/eww/scripts/get-time.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+ACT=$1
 HOUR=$(($(date +%-H) % 12))
 MINUTE=$(date +%-M)
 
-echo $HOUR-$MINUTE
+case "$ACT" in
+    all) date --rfc-3339 seconds
+    ;;
+    time) echo $HOUR-$MINUTE
+    ;;
+    *) echo invalid input; exit 1
+    ;;
+esac
+
+
