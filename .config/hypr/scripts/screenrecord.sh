4258780387f28ddbd18cc04f76afe2fabd22b102
sytjason <jasont1020508@gmail.com>
2025-03-09 18:10:02 +0800

    [eww/hypr] add screen record icon

--- .config/hypr/scripts/screenrecord.sh
+++ .config/hypr/scripts/screenrecord.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+notif_and_blink() {
+    notify-send "Recording started"
+    eww update show_rec=true
+    while true; do
+        sleep 0.5
+        eww update blink_rec=true
+        sleep 0.5
+        eww update blink_rec=false
+    done
+}
+
 if [ "$(pgrep wf-recorder)" = "" ]; then
-    wf-recorder -g "$(slurp)" -f $HOME/Videos/recording-$(date +%G-%m-%d-%H%M%S).mkv
+    region=$(slurp)
+    notif_and_blink &
+    wf-recorder -g "$region" -f $HOME/Videos/recording-$(date +%G-%m-%d-%H%M%S).mkv
+    exit 0
 else
+    notify-send "Recording stopped"
+    eww update show_rec=false
     killall wf-recorder
 fi
 
