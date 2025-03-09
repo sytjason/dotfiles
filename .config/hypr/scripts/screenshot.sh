5b97d6222ca2393a088f8eb6cc587da5e5121bfc
sytjason <jasont1020508@gmail.com>
2025-03-09 18:10:45 +0800

    [hypr] send notification while screenshot

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -3,9 +3,13 @@
 ACT=$1
 
 case "$ACT" in
-    cp) grim -g "$(slurp)" - | wl-copy
+    cp) 
+        grim -g "$(slurp)" - | wl-copy
+        notify-send "Screenshot copied to clipboard"
     ;;
-    save) grim -g "$(slurp)"  ~/Pictures/screenshot-$(date +%G-%m-%e-%H%M%S).png
+    save)
+        grim -g "$(slurp)"  ~/Pictures/screenshot-$(date +%G-%m-%e-%H%M%S).png
+        notify-send "Screenshot saved to ~/Pictures"
     ;;
     *) echo invalid action: $ACT
     ;;
