4fb44da09f066e6ea12eeebbfdd65572dc89f2bd
sytjason <jasont1020508@gmail.com>
2025-04-06 15:32:39 +0800

    [eww] correct volume mute status

--- .config/eww/scripts/get-volume.sh
+++ .config/eww/scripts/get-volume.sh
@@ -4,14 +4,15 @@ LAST_VOLUME=$(pamixer --get-volume)
 pactl subscribe | grep --line-buffered sink |
 while read -r _; do
     NEW_VOLUME=$(pamixer --get-volume)
-    echo $NEW_VOLUME
+    IS_MUTE=$(pamixer --get-mute)
+    echo "{\"volume\": $NEW_VOLUME, \"mute\": $IS_MUTE}"
     DIFF=$((NEW_VOLUME - LAST_VOLUME))
-    if [[ $NEW_VOLUME -eq 0 ]]; then
-        notify-send "" -t 1000 -i $HOME/.config/eww/volume/volume-no.svg
+    if [[ $NEW_VOLUME -eq 0 ]] || $IS_MUTE; then
+        notify-send "Volume: mute" -t 1000 -i $HOME/.config/eww/volume/volume-no.svg
     elif [[ $DIFF -gt 0 ]]; then
-        notify-send "" -t 1000 -i $HOME/.config/eww/volume/volume-up.svg
+        notify-send "Volume: $NEW_VOLUME%" -t 1000 -i $HOME/.config/eww/volume/volume-up.svg
     elif [[ $DIFF -lt 0 ]]; then
-        notify-send "" -t 1000 -i $HOME/.config/eww/volume/volume-down.svg
+        notify-send "Volume: $NEW_VOLUME%" -t 1000 -i $HOME/.config/eww/volume/volume-down.svg
     fi
     LAST_VOLUME=$NEW_VOLUME
 done
